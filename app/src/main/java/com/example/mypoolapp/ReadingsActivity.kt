f5fa3e47b77e60ce2fcc6051caeb074822d150ed
kristian-montoya <kristian.s.montoya@gmail.com>
2020-06-28 20:19:01 -0600

    code cleanup
    Adding functions to remove repeated code. Also changed layout of readings page to be built solely off of card views

--- app/src/main/java/com/example/mypoolapp/ReadingsActivity.kt
+++ app/src/main/java/com/example/mypoolapp/ReadingsActivity.kt
@@ -1,65 +1,93 @@
 package com.example.mypoolapp
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
-import android.widget.ToggleButton
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
-private var vesselType = "pool"
+
+private  var vesselType: String = "pool"
 
 class ReadingsActivity : AppCompatActivity(){
 
     fun getVesselType(): String{ return vesselType }
 
-    fun activatePoolPage(){
+    fun setVesselType(ves: String){ vesselType = ves }
+
+    fun activatePool(){
         println("Activating pool profile!")
-        vesselType = "pool"
+        setVesselType("pool")
+        reading_page.setBackgroundColor(Color.argb(117,21,96,103))
+        pool_button.setBackgroundColor(Color.TRANSPARENT)
+        pool_button.setTextColor(Color.WHITE)
+    }
+
+    fun deactivatePool(){
+        println("Deactivating pool profile!")
+        pool_button.toggle()
+        pool_button.setBackgroundColor(Color.GRAY)
+        pool_button.setTextColor(Color.BLACK)
+
+    }
+
+    fun activateSpa(){
+        println("Activating spa profile!")
+        setVesselType("spa")
+        reading_page.setBackgroundColor(Color.argb(117,129,0,0))
+        spa_button.setBackgroundColor(Color.TRANSPARENT)
+        spa_button.setTextColor(Color.WHITE)
+    }
+
+    fun deactivateSpa(){
+        println("Deactivating spa profile!")
         spa_button.toggle()
-        reading_page.setBackgroundColor(Color.CYAN)
-        pool_button.setBackgroundColor(Color.CYAN)
+        spa_button.setBackgroundColor(Color.GRAY)
+        spa_button.setTextColor(Color.BLACK)
+    }
+
+    fun phEntryHandler(input: CharSequence){
+        println(input)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        reading_page.setBackgroundColor(Color.CYAN)
-
+        activatePool() //set page with pool profile selected by default
+        deactivateSpa() //make sure spa profile is deactivated
         pool_button.setOnClickListener{
-            d("pool_button", "Pool button pressed!")
-            if(vesselType == "pool"){
-                d("pool_button", "Pool button already active!")
+            println("Pool button pressed!")
+            if(getVesselType() == "pool"){
+                println("Pool button already active!")
                 pool_button.toggle() //cancel out the initial button press
             }
             else{
-                activatePoolPage()
-                spa_button.setBackgroundColor(Color.GRAY)
-                d("spa_button", "Deactivating spa button!")
+                activatePool()
+                deactivateSpa()
             }
-            d("pool_button", vesselType)
+            println(getVesselType())
         }
-
         spa_button.setOnClickListener{
-            d("spa_button", "Spa button pressed!")
-            if(vesselType == "spa"){
-                d("spa_button", "Spa button already active!")
+            println("Spa button pressed!")
+            if(getVesselType() == "spa"){
+                println("Spa button already active!")
                 spa_button.toggle() //cancel out the initial button press
             }
             else{
-                d("spa_button", "Activating spa button!")
-                vesselType = "spa"
-                pool_button.toggle()
-                d("pool_button", "Deactivating pool button!")
-                pool_button.setBackgroundColor(Color.GRAY)
-                reading_page.setBackgroundColor(Color.RED)
-                spa_button.setBackgroundColor(Color.RED)
+                activateSpa()
+                deactivatePool()
             }
-            d("spa_button", vesselType)
+            println(getVesselType())
         }
 
+        ph_entry.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                phEntryHandler(s)
+            }
+        })
     }
 
 }
