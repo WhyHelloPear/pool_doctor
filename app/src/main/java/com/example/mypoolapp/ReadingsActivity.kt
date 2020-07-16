0c981c585ea872d335147893cca1babe24e86763
kristian-montoya <kristian.s.montoya@gmail.com>
2020-07-15 22:30:09 -0600

    small update
    Small update to include a simple data structure of each element we are tracking for the pool's water chemistry.

--- app/src/main/java/com/example/mypoolapp/ReadingsActivity.kt
+++ app/src/main/java/com/example/mypoolapp/ReadingsActivity.kt
@@ -11,9 +11,12 @@ import android.widget.EditText
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.reflect.typeOf
 
 
 class ReadingsActivity : AppCompatActivity(){
+
+    data class Element(val name: String, val card_body: TableRow, val card_entry: EditText, val full_range: List<Float>, val ideal_range: List<Float>) {}
 
     private  var vesselType: String = "pool"
     private var ph_reading: Float = 0f
@@ -23,6 +26,9 @@ class ReadingsActivity : AppCompatActivity(){
     private var ca_reading: Int = 0
     private var cya_reading: Int = 0
     private var phos_reading: Int = 0
+
+    val ph_range = listOf(7f,8f)
+
 
     private lateinit var active_body: TableRow
 
@@ -40,24 +46,25 @@ class ReadingsActivity : AppCompatActivity(){
 
     private fun hideView(view: View){ view.visibility = View.GONE }
 
-    private fun showView(view: TableRow, entry: View){
-        view.visibility = View.VISIBLE
-        active_body = view
-        entry.requestFocus()
+    private fun showView(element: Element){
+        element.card_body.visibility = View.VISIBLE
+        active_body = element.card_body
+        element.card_entry.requestFocus()
     }
 
-    private fun toggleActiveBody(body: TableRow, entry: EditText){
-        if(body.visibility == View.VISIBLE) {
-            hideView(body)
-            showView(page_header, page_entry)
-            disableKeyboard(entry)
+    private fun toggleActiveBody(element: Element){
+        if(element.card_body.visibility == View.VISIBLE) {
+            hideView(element.card_body)
+            showView(element)
+            disableKeyboard(element.card_body)
         }
         else{
             if(active_body != page_header){
                 hideView(active_body)
+
             }
-            showView(body, entry)
-            enableKeyboard(entry)
+            showView(element)
+            enableKeyboard(element.card_entry)
         }
     }
 
@@ -133,6 +140,15 @@ class ReadingsActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val ph_element:Element = Element("ph", ph_info_body, ph_entry, listOf(7f,8f), listOf(7.4f,7.6f))
+        val chlor_element:Element = Element("chlor", chlor_info_body, chlor_entry, listOf(0f,10f), listOf(7.4f,7.6f))
+        val alk_element:Element = Element("alk", alk_info_body, alk_entry, listOf(0f,1000f), listOf(80f,120f))
+        val ca_element:Element = Element("ca", ca_info_body, ca_entry, listOf(0f,1000f), listOf(200f,400f))
+        val cya_element:Element = Element("cya", cya_info_body, cya_entry, listOf(0f,100f), listOf(30f,80f))
+        val phos_element:Element = Element("phos", phos_info_body, phos_entry, listOf(0f,1000f), listOf(0f,0f))
+
+
+
         active_body = page_header
 
         activatePool() //set page with pool profile selected by default
@@ -141,49 +157,48 @@ class ReadingsActivity : AppCompatActivity(){
         pool_button.setOnClickListener{ profileHandler("pool") }
         spa_button.setOnClickListener{ profileHandler("spa" ) }
 
-        ph_card_click_area.setOnClickListener(){ toggleActiveBody(ph_info_body, ph_entry) }
-        ph_info_button.setOnClickListener(){ toggleActiveBody(ph_info_body, ph_entry) }
-        ph_info_body.setOnClickListener(){ toggleActiveBody(ph_info_body, ph_entry) }
+        ph_card_click_area.setOnClickListener(){ toggleActiveBody(ph_element) }
+        ph_info_button.setOnClickListener(){ toggleActiveBody(ph_element) }
+        ph_info_body.setOnClickListener(){ toggleActiveBody(ph_element) }
 
-        chlor_card_click_area.setOnClickListener(){ toggleActiveBody(chlor_info_body, chlor_entry) }
-        chlor_info_button.setOnClickListener(){ toggleActiveBody(chlor_info_body, chlor_entry) }
-        chlor_info_body.setOnClickListener(){ toggleActiveBody(chlor_info_body, chlor_entry) }
+        chlor_card_click_area.setOnClickListener(){ toggleActiveBody(chlor_element) }
+        chlor_info_button.setOnClickListener(){ toggleActiveBody(chlor_element) }
+        chlor_info_body.setOnClickListener(){ toggleActiveBody(chlor_element) }
 
-        alk_card_click_area.setOnClickListener(){ toggleActiveBody(alk_info_body, alk_entry) }
-        alk_info_button.setOnClickListener(){ toggleActiveBody(alk_info_body, alk_entry) }
-        alk_info_body.setOnClickListener(){ toggleActiveBody(alk_info_body, alk_entry) }
+        alk_card_click_area.setOnClickListener(){ toggleActiveBody(alk_element) }
+        alk_info_button.setOnClickListener(){ toggleActiveBody(alk_element) }
+        alk_info_body.setOnClickListener(){ toggleActiveBody(alk_element) }
 
-        ca_card_click_area.setOnClickListener(){ toggleActiveBody(ca_info_body, ca_entry) }
-        ca_info_button.setOnClickListener(){ toggleActiveBody(ca_info_body, ca_entry) }
-        ca_info_body.setOnClickListener(){ toggleActiveBody(ca_info_body, ca_entry) }
+        ca_card_click_area.setOnClickListener(){ toggleActiveBody(ca_element) }
+        ca_info_button.setOnClickListener(){ toggleActiveBody(ca_element) }
+        ca_info_body.setOnClickListener(){ toggleActiveBody(ca_element) }
 
-        cya_card_click_area.setOnClickListener(){ toggleActiveBody(cya_info_body, cya_entry) }
-        cya_info_button.setOnClickListener(){ toggleActiveBody(cya_info_body, cya_entry) }
-        cya_info_body.setOnClickListener(){ toggleActiveBody(cya_info_body, cya_entry) }
+        cya_card_click_area.setOnClickListener(){ toggleActiveBody(cya_element) }
+        cya_info_button.setOnClickListener(){ toggleActiveBody(cya_element) }
+        cya_info_body.setOnClickListener(){ toggleActiveBody(cya_element) }
 
-        phos_card_click_area.setOnClickListener(){ toggleActiveBody(phos_info_body, phos_entry) }
-        phos_info_button.setOnClickListener(){ toggleActiveBody(phos_info_body, phos_entry) }
-        phos_info_body.setOnClickListener { toggleActiveBody(phos_info_body, phos_entry) }
+        phos_card_click_area.setOnClickListener(){ toggleActiveBody(phos_element) }
+        phos_info_button.setOnClickListener(){ toggleActiveBody(phos_element) }
+        phos_info_body.setOnClickListener { toggleActiveBody(phos_element) }
 
-        ph_entry.addTextChangedListener(object: TextWatcher {
-            var orig:CharSequence = ""
-            override fun afterTextChanged(s: Editable) {
-                println(orig)
-                println(s)
-            }
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-                orig = s
-                println("before $s")
-                println("orig: $orig")
-            }
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                print("start: $start, before: $before")
-                println("after $s")
-                val ph_range = listOf(7f,8f)
-                entryHandler(s, ph_entry, ph_range)
-            }
-
-        })
+//        ph_entry.addTextChangedListener(object: TextWatcher {
+//            var orig:CharSequence = ""
+//            override fun afterTextChanged(s: Editable) {
+//                println(orig)
+//                println(s)
+//            }
+//            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+//                orig = s
+//                println("before $s")
+//                println("orig: $orig")
+//            }
+//            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+//                print("start: $start, before: $before")
+//                println("after $s"
+//                entryHandler(s, ph_entry, ph_range)
+//            }
+//
+//        })
 
 
     }
