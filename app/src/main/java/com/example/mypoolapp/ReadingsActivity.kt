22f6529900df43bbc9964bf6efd64979e0ed5f5b
kristian-montoya <kristian.s.montoya@gmail.com>
2020-07-18 20:16:29 -0600

    functionality update
    Implemented EditText functionality on input fields.

--- app/src/main/java/com/example/mypoolapp/ReadingsActivity.kt
+++ app/src/main/java/com/example/mypoolapp/ReadingsActivity.kt
@@ -5,7 +5,9 @@ import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TableRow
@@ -16,16 +18,11 @@ import kotlin.reflect.typeOf
 
 class ReadingsActivity : AppCompatActivity(){
 
-    data class Element(val name: String, val card_body: TableRow, val card_entry: EditText, val full_range: List<Float>, val ideal_range: List<Float>) {}
+    data class Element(val name: String,val card: View, val card_body: View, val card_entry: EditText, val full_range: List<Float>, val ideal_range: List<Float>) {}
 
-//    val defaultElement:Element = Element("default", page_header, page_entry, listOf(0f,0f), listOf(0f,0f))
-
-
-
-    private lateinit var defaultElement:Element
+    private lateinit var vesselType:String
     private lateinit var activeElement:Element
-
-    private  var vesselType: String = "pool"
+    private lateinit var defaultElement:Element
 
 
     private fun getIMM(): InputMethodManager{ return getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager }
@@ -84,6 +81,9 @@ class ReadingsActivity : AppCompatActivity(){
                 pool_button.toggle() //cancel out the initial button press
             }
             else{
+                disableKeyboard(defaultElement.card_entry)
+                cya_card.visibility = View.VISIBLE
+                phos_card.visibility = View.VISIBLE
                 activatePool()
                 deactivateSpa()
             }
@@ -94,17 +94,14 @@ class ReadingsActivity : AppCompatActivity(){
                 println("Spa button already active!")
                 spa_button.toggle() //cancel out the initial button press
             } else {
+                disableKeyboard(defaultElement.card_entry)
+                cya_card.visibility = View.GONE
+                phos_card.visibility = View.GONE
                 activateSpa()
                 deactivatePool()
             }
         }
     }
-
-//    private fun entryHandler(input: CharSequence, entry: EditText, range: List<Float>){
-//        val t = entry.text
-//        println("input from parameter: $input")
-//        println("Text currently in box: $t")
-//    }
 
     private fun hideElement(element: Element){ element.card_body.visibility = View.GONE }
 
@@ -114,17 +111,37 @@ class ReadingsActivity : AppCompatActivity(){
         element.card_entry.requestFocus()
     }
 
-
-
 // When to check if a user has entered a number
     // 1) When card gets activated -----> Check the previous active element
         // If the previous active element is NOT our default value, check if the previous element's entry has changed
 
     // 2) When card get deactivated
 
-    // 3) When "next" OR "done" button is pressed for the text entries
+    // 3) When "next" OR "done" button is pressed for the text entries (done with onAction function)
+
+    // 4) When user clicks between text entries
 
 
+
+
+    private fun handleEntry(element: Element) {
+        val holder:String = element.card_entry.text.toString()
+        if(holder == "."){
+            element.card_entry.setText("")
+        }
+        else if(holder != ""){
+            var entryValue:Float = 0f
+            if("≤" in holder){ entryValue = element.full_range[0] }
+            else if("≥" in holder){ entryValue = element.full_range[1] }
+            else{ entryValue = holder.toFloat() }
+            if(entryValue >= element.full_range[1]){
+                element.card_entry.setText("≥${element.full_range[1].toInt()}")
+            }
+            else if(entryValue <= element.full_range[0]){
+                element.card_entry.setText("≤${element.full_range[0].toInt()}")
+            }
+        }
+    }
 
 
 
@@ -140,7 +157,9 @@ class ReadingsActivity : AppCompatActivity(){
         else{ // if the selected card is NOT visible, we need to switch active card bodies
             if(activeElement != defaultElement){ //check if the active body is our default preset; if it's not, we need to hide the current active body
                 hideElement(activeElement) //close the active card body so no card is open/selected
+                handleEntry(activeElement)
             }
+            activeElement = element
             showElement(element) //activate the selected card body
             enableKeyboard(element.card_entry) //bring up the keyboard so user can type shit
         }
@@ -153,14 +172,14 @@ class ReadingsActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val phElement:Element = Element("ph", ph_info_body, ph_entry, listOf(7f,8f), listOf(7.4f,7.6f))
-        val chlorElement:Element = Element("chlor", chlor_info_body, chlor_entry, listOf(0f,10f), listOf(7.4f,7.6f))
-        val alkElement:Element = Element("alk", alk_info_body, alk_entry, listOf(0f,1000f), listOf(80f,120f))
-        val caElement:Element = Element("ca", ca_info_body, ca_entry, listOf(0f,1000f), listOf(200f,400f))
-        val cyaElement:Element = Element("cya", cya_info_body, cya_entry, listOf(0f,100f), listOf(30f,80f))
-        val phosElement:Element = Element("phos", phos_info_body, phos_entry, listOf(0f,1000f), listOf(0f,0f))
+        val phElement:Element = Element("ph", ph_card, ph_info_body, ph_entry, listOf(7f,8f), listOf(7.4f,7.6f))
+        val chlorElement:Element = Element("chlor", chlor_card, chlor_info_body, chlor_entry, listOf(0f,10f), listOf(7.4f,7.6f))
+        val alkElement:Element = Element("alk", alk_card, alk_info_body, alk_entry, listOf(0f,999f), listOf(80f,120f))
+        val caElement:Element = Element("ca", ca_card, ca_info_body, ca_entry, listOf(0f,999f), listOf(200f,400f))
+        val cyaElement:Element = Element("cya", cya_card, cya_info_body, cya_entry, listOf(0f,400f), listOf(30f,80f))
+        val phosElement:Element = Element("phos", phos_card, phos_info_body, phos_entry, listOf(0f,999f), listOf(0f,0f))
 
-        defaultElement = Element("default", page_header, page_entry, listOf(0f,0f), listOf(0f,0f))
+        defaultElement = Element("default", page_header, page_body, page_entry, listOf(0f,0f), listOf(0f,0f))
         activeElement = defaultElement
 
         activatePool() //set page with pool profile selected by default
@@ -169,54 +188,25 @@ class ReadingsActivity : AppCompatActivity(){
         pool_button.setOnClickListener{ profileHandler("pool") }
         spa_button.setOnClickListener{ profileHandler("spa" ) }
 
-        ph_card_click_area.setOnClickListener(){ toggleActiveBody(phElement) }
-        ph_info_button.setOnClickListener(){ toggleActiveBody(phElement) }
-        ph_info_body.setOnClickListener(){ toggleActiveBody(phElement) }
-        ph_entry.setOnClickListener(){toggleActiveBody(phElement)}
 
 
+        ph_card.setOnClickListener(){toggleActiveBody(phElement)}
+        ph_entry.setOnFocusChangeListener{ view, b -> handleEntry(phElement) }
 
+        chlor_card.setOnClickListener(){ toggleActiveBody(chlorElement) }
+        chlor_entry.setOnFocusChangeListener{ view, b -> handleEntry(chlorElement) }
 
-        chlor_card_click_area.setOnClickListener(){ toggleActiveBody(chlorElement) }
-        chlor_info_button.setOnClickListener(){ toggleActiveBody(chlorElement) }
-        chlor_info_body.setOnClickListener(){ toggleActiveBody(chlorElement) }
+        alk_card.setOnClickListener(){ toggleActiveBody(alkElement) }
+        alk_entry.setOnFocusChangeListener{ view, b -> handleEntry(alkElement) }
 
-        alk_card_click_area.setOnClickListener(){ toggleActiveBody(alkElement) }
-        alk_info_button.setOnClickListener(){ toggleActiveBody(alkElement) }
-        alk_info_body.setOnClickListener(){ toggleActiveBody(alkElement) }
+        ca_card.setOnClickListener(){ toggleActiveBody(caElement) }
+        ca_entry.setOnFocusChangeListener{ view, b -> handleEntry(caElement) }
 
-        ca_card_click_area.setOnClickListener(){ toggleActiveBody(caElement) }
-        ca_info_button.setOnClickListener(){ toggleActiveBody(caElement) }
-        ca_info_body.setOnClickListener(){ toggleActiveBody(caElement) }
+        cya_card.setOnClickListener(){ toggleActiveBody(cyaElement) }
+        cya_entry.setOnFocusChangeListener{ view, b -> handleEntry(cyaElement) }
 
-        cya_card_click_area.setOnClickListener(){ toggleActiveBody(cyaElement) }
-        cya_info_button.setOnClickListener(){ toggleActiveBody(cyaElement) }
-        cya_info_body.setOnClickListener(){ toggleActiveBody(cyaElement) }
-
-        phos_card_click_area.setOnClickListener(){ toggleActiveBody(phosElement) }
-        phos_info_button.setOnClickListener(){ toggleActiveBody(phosElement) }
-        phos_info_body.setOnClickListener { toggleActiveBody(phosElement) }
-
-//        ph_entry.addTextChangedListener(object: TextWatcher {
-//            var orig:CharSequence = ""
-//            override fun afterTextChanged(s: Editable) {
-//                println(orig)
-//                println(s)
-//            }
-//            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-//                orig = s
-//                println("before $s")
-//                println("orig: $orig")
-//            }
-//            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-//                print("start: $start, before: $before")
-//                println("after $s"
-//                entryHandler(s, ph_entry, ph_range)
-//            }
-//
-//        })
-
-
+        phos_card.setOnClickListener(){ toggleActiveBody(phosElement) }
+        phos_entry.setOnFocusChangeListener{ view, b -> handleEntry(phosElement) }
     }
 
 }
